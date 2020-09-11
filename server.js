f5cac21047a52652b1b9b23d4a695d0ba6aa426a
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-11 16:07:54 -0400

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const apolloServer = require('apollo-server-express')
 const db = require('./db/config').uri;
+// const resolvers = require("./app/Grapgql/resolvers.ts");
+// const typedefs = require("./app/Grapgql/typedef.ts");
+
 const app = express();
+
+// const server = new ApolloServer({ typeDefs: typedefs, resolvers });
+// server.applyMiddleware({ app, path: "/api" });
 
 mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true  } )
 .then(console.log("connected"))
